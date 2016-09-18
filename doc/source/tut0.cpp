9b8e500bb63df38b10de6d8ecfecec9edf0f0cd9
Oliver Nagy <olitheolix@gmail.com>
2016-09-18 14:28:17 +1000

    Compiler tutorial now prints Newton's version number.

--- doc/source/tut0.cpp
+++ doc/source/tut0.cpp
@@ -4,9 +4,12 @@
 
 int main (int argc, const char * argv[])
 {
+  // Print the library version.
+  printf("Hello, this is Newton version %d\n", NewtonWorldGetVersion());
+
   // Create the Newton world.
   NewtonWorld* const world = NewtonCreate();
-	
+
   // Step the (empty) world 60 times in increments of 1/60 second.
   const float timestep = 1.0f / 60;
   for(int i=0; i<60; i++) {
